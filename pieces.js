a88d68b456e2e83063760431f17a05076a605ec8
Kromaric <romaricyt11@gmail.com>
2025-01-18 02:53:42 +0100

    Ajout du tri et d'un filtreur

--- pieces.js
+++ pieces.js
@@ -37,3 +37,25 @@ for (let i = 0; i < pieces.length; i++) {
 
  }
  
+
+// Gestion des boutons
+const boutonTrier = document.querySelector(".btn-trier");
+
+boutonTrier.addEventListener("click", function () {
+  const piecesOrdonnees = Array.from(pieces);
+
+  piecesOrdonnees.sort(function (a, b) {
+    return a.prix - b.prix;
+  });
+
+  console.log(piecesOrdonnees);
+});
+
+
+const boutonFiltrer = document.querySelector(".btn-filtrer");
+
+boutonFiltrer.addEventListener("click", function () {
+   const piecesFiltrees = pieces.filter(function (piece) {
+       return piece.prix <= 35;
+   });
+  }) ;
